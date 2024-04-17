37b9a8748de0eb415c2d64be8988bb4419eae0b6
b23011448 <abderrahmane.bengherbia@etu.univ-amu.fr>
2024-04-17 17:35:05 +0200

    nouveux

--- src/main/java/fr/amu/iut/exercice4/Palette.java
+++ src/main/java/fr/amu/iut/exercice4/Palette.java
@@ -1,8 +1,11 @@
 package fr.amu.iut.exercice4;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
@@ -25,6 +28,31 @@ public class Palette extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+    BorderPane root = new BorderPane();
+    Pane page = new Pane();
+    HBox zoneL = new HBox();
+    Label label = new Label("couleur");
+    zoneL.getChildren().add(label);
+    HBox zoneB = new HBox();
+    Button Vert = new Button("Vert");
+    Button Rouge = new Button("Rouge");
+    Button Bleu = new Button("Vert");
+
+    zoneB.getChildren().addAll(Vert,Rouge,Bleu);
+    zoneB.setSpacing(10);
+    zoneB.setAlignment(Pos.CENTER);
+    root.setBottom(zoneB);
+    zoneL.setAlignment(Pos.CENTER);
+    root.setTop(zoneL);
+    Vert.addEventFilter(MouseEvent.MOUSE_CLICKED,actionEvent -> {page.setStyle("-fx-background-color:Green");label.setText("vert");});
+    Rouge.addEventFilter(MouseEvent.MOUSE_CLICKED,actionEvent -> {page.setStyle("-fx-background-color:Red");label.setText("rouge");});
+    Bleu.addEventFilter(MouseEvent.MOUSE_CLICKED,actionEvent -> {page.setStyle("-fx-background-color:Blue");label.setText("bleu");});
+    root.setCenter(page);
+    Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setHeight(200);
+        primaryStage.setWidth(400);
         primaryStage.show();
     }
 }
